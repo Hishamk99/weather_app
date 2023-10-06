ebab8e9fc759aafe8e59e89fa3bd6d0258c6b0bd
Hishamk99 <hishamkamel370@gmail.com>
2023-10-06 19:25:05 +0200

    refactor in weather service

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -4,9 +4,11 @@ import 'package:weather_app/models/weather_model.dart';
 class WeatherServices {
   final Dio dio;
   WeatherServices(this.dio);
+  final String baseUrl = 'https://api.weatherapi.com/v1';
+  final String apiKey = '126986aafbd64e2492a150217230610';
   Future<WeatherModel> getCurrentWeather({required String cityName}) async {
    Response response = await dio.get(
-        'https://api.weatherapi.com/v1/forecast.json?key=126986aafbd64e2492a150217230610&q=$cityName&days=1');
+        '$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1');
     WeatherModel weatherModel = WeatherModel.fromJson(response.data);
     return weatherModel;
   }
