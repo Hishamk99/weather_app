70053c3b753382ca436975ea346170fff3a27c35
Hishamk99 <hishamkamel370@gmail.com>
2023-10-09 11:46:26 +0200

    change backgroung color

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_state.dart';
-import 'package:weather_app/services/weather_service.dart';
 import 'views/home_page.dart';
 
 void main() {
@@ -33,72 +32,72 @@ class WeatherApp extends StatelessWidget {
       ),
     );
   }
+}
 
-  MaterialColor getThemeColor(String? condition) {
-    if (condition == null) {
+MaterialColor getThemeColor(String? condition) {
+  if (condition == null) {
+    return Colors.blue;
+  }
+  switch (condition) {
+    case 'Sunny':
+    case 'Clear':
+      return Colors.orange;
+    case 'Partly cloudy':
       return Colors.blue;
-    }
-    switch (condition) {
-      case 'Sunny':
-      case 'Clear':
-        return Colors.orange;
-      case 'Partly cloudy':
-        return Colors.blue;
-      case 'Cloudy':
-      case 'Overcast':
-      case 'Mist':
-      case 'Fog':
-      case 'Freezing fog':
-        return Colors.grey;
+    case 'Cloudy':
+    case 'Overcast':
+    case 'Mist':
+    case 'Fog':
+    case 'Freezing fog':
+      return Colors.grey;
 
-      case 'Thundery outbreaks possible':
-        return Colors.deepPurple;
+    case 'Thundery outbreaks possible':
+      return Colors.deepPurple;
 
-      case 'Patchy rain possible':
-      case 'Patchy snow possible':
-      case 'Patchy sleet possible':
-      case 'Patchy freezing drizzle possible':
-      case 'Blowing snow':
-      case 'Blizzard':
-      case 'Patchy light drizzle':
-      case 'Light drizzle':
-      case 'Freezing drizzle':
-      case 'Heavy freezing drizzle':
-      case 'Patchy light rain':
-      case 'Light rain':
-      case 'Moderate rain at times':
-      case 'Moderate rain':
-      case 'Heavy rain at times':
-      case 'Heavy rain':
-      case 'Light freezing rain':
-      case 'Moderate or heavy freezing rain':
-      case 'Light sleet':
-      case 'Moderate or heavy sleet':
-      case 'Patchy light snow':
-      case 'Light snow':
-      case 'Patchy moderate snow':
-      case 'Moderate snow':
-      case 'Patchy heavy snow':
-      case 'Heavy snow':
-      case 'Ice pellets':
-      case 'Light rain shower':
-      case 'Moderate or heavy rain shower':
-      case 'Torrential rain shower':
-      case 'Light sleet showers':
-      case 'Moderate or heavy sleet showers':
-      case 'Light snow showers':
-      case 'Moderate or heavy snow showers':
-      case 'Light showers of ice pellets':
-      case 'Moderate or heavy showers of ice pellets':
-        return Colors.lightBlue;
+    case 'Patchy rain possible':
+    case 'Patchy snow possible':
+    case 'Patchy sleet possible':
+    case 'Patchy freezing drizzle possible':
+    case 'Blowing snow':
+    case 'Blizzard':
+    case 'Patchy light drizzle':
+    case 'Light drizzle':
+    case 'Freezing drizzle':
+    case 'Heavy freezing drizzle':
+    case 'Patchy light rain':
+    case 'Light rain':
+    case 'Moderate rain at times':
+    case 'Moderate rain':
+    case 'Heavy rain at times':
+    case 'Heavy rain':
+    case 'Light freezing rain':
+    case 'Moderate or heavy freezing rain':
+    case 'Light sleet':
+    case 'Moderate or heavy sleet':
+    case 'Patchy light snow':
+    case 'Light snow':
+    case 'Patchy moderate snow':
+    case 'Moderate snow':
+    case 'Patchy heavy snow':
+    case 'Heavy snow':
+    case 'Ice pellets':
+    case 'Light rain shower':
+    case 'Moderate or heavy rain shower':
+    case 'Torrential rain shower':
+    case 'Light sleet showers':
+    case 'Moderate or heavy sleet showers':
+    case 'Light snow showers':
+    case 'Moderate or heavy snow showers':
+    case 'Light showers of ice pellets':
+    case 'Moderate or heavy showers of ice pellets':
+      return Colors.lightBlue;
 
-      case 'Patchy light rain with thunder':
-      case 'Moderate or heavy rain with thunder':
-      case 'Patchy light snow with thunder':
-      case 'Moderate or heavy snow with thunder':
-        return Colors.deepPurple;
-      default:
-        return Colors.blue;
-    }
+    case 'Patchy light rain with thunder':
+    case 'Moderate or heavy rain with thunder':
+    case 'Patchy light snow with thunder':
+    case 'Moderate or heavy snow with thunder':
+      return Colors.deepPurple;
+    default:
+      return Colors.blue;
   }
 }
--- lib/widgets/weather_info_body.dart
+++ lib/widgets/weather_info_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:weather_app/main.dart';
 import 'package:weather_app/models/weather_model.dart';
 
 class WeatherInfoBody extends StatelessWidget {
@@ -8,47 +9,61 @@ class WeatherInfoBody extends StatelessWidget {
   final WeatherModel weather;
   @override
   Widget build(BuildContext context) {
-    var weatherModel = BlocProvider.of<GetWeatherCubit>(context).weatherModel!;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            weatherModel.cityName,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-          ),
-          Text(
-            'Updated at ${weatherModel.date.hour}:${weatherModel.date.minute}',
-            style: const TextStyle(fontSize: 24),
-          ),
-          const SizedBox(
-            height: 32,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Image.network('https:${weatherModel.image}'),
-              Text(
-                weatherModel.temp.toString(),
-                style:
-                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-              ),
-              Column(
-                children: [
-                  Text('Maxtemp : ${weatherModel.maxTemp.round()}',
-                      style: const TextStyle(fontSize: 16)),
-                  Text('Mintemp : ${weatherModel.minTemp.round()}',
-                      style: const TextStyle(fontSize: 16)),
-                ],
-              ),
-            ],
-          ),
-          Text(
-            weatherModel.condition,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
-          ),
-        ],
+    WeatherModel weatherModel =
+        BlocProvider.of<GetWeatherCubit>(context).weatherModel!;
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            getThemeColor(weatherModel.condition),
+            getThemeColor(weatherModel.condition)[300]!,
+            getThemeColor(weatherModel.condition)[50]!,
+          ],
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              weatherModel.cityName,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+            Text(
+              'Updated at ${weatherModel.date.hour}:${weatherModel.date.minute}',
+              style: const TextStyle(fontSize: 24),
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.network('https:${weatherModel.image}'),
+                Text(
+                  weatherModel.temp.toString(),
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 30),
+                ),
+                Column(
+                  children: [
+                    Text('Maxtemp : ${weatherModel.maxTemp.round()}',
+                        style: const TextStyle(fontSize: 16)),
+                    Text('Mintemp : ${weatherModel.minTemp.round()}',
+                        style: const TextStyle(fontSize: 16)),
+                  ],
+                ),
+              ],
+            ),
+            Text(
+              weatherModel.condition,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+            ),
+          ],
+        ),
       ),
     );
   }
